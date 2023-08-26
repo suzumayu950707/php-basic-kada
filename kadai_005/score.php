4aecc4772158912b008e4c56ce839ec71739625b
suzumayu950707 <143255504+suzumayu950707@users.noreply.github.com>
2023-08-26 18:03:07 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,15 @@
+<?php
+    $score01 = 80 ;
+    $score02 = 60 ;
+    $score03 = 55 ;
+    $score04 = 40 ;
+    $score05 = 100 ;
+    $score06 = 25;
+    $score07 = 80 ;
+    $score08 = 95 ;
+    $score09 = 30 ;
+    $score10 = 60 ;
+
+    echo ($score01+$score02+$score03+$score04+$score05+$score06+$score07+$score08+$score09+$score10)/10 ;
+    ?>
+    
