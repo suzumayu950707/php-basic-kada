14465e6c9dd3fad5756c9617716bfddebeb7cb9f
suzumayu950707 <143255504+suzumayu950707@users.noreply.github.com>
2023-08-27 13:16:43 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>連想配列の課題</title>
+</head>
+<body>
+    <?php
+     $array =['name' => 'onion','price' => 200, 'weight'=>160];
+     print_r($array);
+    ?>
+    
+</body>
+</html>
